09007081b655908e92b7749076bb17f949d58c06
lee_jun_young <ljy03@pusan.ac.kr>
2024-10-07 09:50:00 +0900

    OrderDetail fin

--- app/src/main/java/com/example/coneez2/page/OrderDetail.kt
+++ app/src/main/java/com/example/coneez2/page/OrderDetail.kt
@@ -1,5 +1,7 @@
 package com.example.coneez2.page
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -34,13 +36,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coneez2.R
 import com.example.coneez2.components.CustomTopBar
 import com.example.coneez2.ui.theme.Main600
 
@@ -77,7 +84,12 @@ fun OrderDetailContent(){
     Column {
         ScrollableButtonRow()
 
-        OrderCard("2026.11.11")
+        OrderCard(R.drawable.coffee1,"2026.11.11", "구매확정", "100% 아라비카 블렌드 바라던허니 스페셜티", "13,800원")
+
+        Divider(color = Color(0xFFF1F2F3), thickness = 1.dp)  // 하단에 회색 구분선 추가
+
+        OrderCard(R.drawable.coffee2,"2026.11.02", "배송완료", "에티오피아 코케허니 예가체프G1 스페셜티", "12,200원")
+
     }
 }
 
@@ -139,49 +151,124 @@ fun ScrollableButton(text: String, selectedButton: MutableState<String>) {
 }
 
 @Composable
-fun OrderCard(Date : String){
+fun OrderCard(@DrawableRes imageRes : Int, Date : String, OrderState :String, Name : String, Price : String){
     Box(
         modifier = Modifier
-            .padding(horizontal = 20.dp, vertical = 24.dp)
+            .padding(horizontal = 20.dp)
+            .padding(top = 24.dp)
             .height(200.dp)
             .fillMaxWidth(),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = Date,
-                style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-
+        Column {
+            //첫줄
             Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "주문상세",
+                    text = Date,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+
+                //주문상세
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "주문상세",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Medium
+                        ),
+                        color = Color.Gray
+                    )
+                    IconButton(
+                        onClick = { },
+                        modifier = Modifier.size(24.dp),  // 버튼 크기를 명시적으로 설정
+
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowRight,  // 네비게이션 아이콘 (왼쪽 아이콘)
+                            contentDescription = "Back",
+                            modifier = Modifier.size(24.dp),
+                            tint = Color.LightGray
+                        )
+                    }
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            //구매확정
+            Box {
+                Text(
+                    text = OrderState,
                     style = TextStyle(
                         fontSize = 14.sp,
                         fontWeight = FontWeight.Medium
                     ),
-                    color = Color.Gray
+                    color = Color(0xFF68BC43)
                 )
-                IconButton(
-                    onClick = { },
-                    modifier = Modifier.size(24.dp),  // 버튼 크기를 명시적으로 설정
+            }
 
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.KeyboardArrowRight,  // 네비게이션 아이콘 (왼쪽 아이콘)
-                        contentDescription = "Back",
-                        modifier = Modifier.size(24.dp),
-                        tint = Color.LightGray
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "oreder1",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(85.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column {
+                    Text(
+                        text = Name,
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF303236),
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
                     )
+
+                    Spacer(modifier = Modifier.height(1.dp))
+
+
+                    Text(
+                        text = "100g / 1개",
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            lineHeight = 16.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF60646C),
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(1.dp))
+
+                    Text(
+                        text = Price,
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            lineHeight = 18.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF303236),
+                        ),
+
+                    )
+
                 }
 
             }
