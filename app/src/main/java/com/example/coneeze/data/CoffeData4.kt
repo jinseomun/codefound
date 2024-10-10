bcb3144351fedd5642505225ab67f4796fdae8e3
lee_jun_young <ljy03@pusan.ac.kr>
2024-10-11 02:53:11 +0900

    머지 중 2

--- app/src/main/java/com/example/coneeze/data/CoffeData4.kt
+++ app/src/main/java/com/example/coneeze/data/CoffeData4.kt
@@ -1,24 +1,20 @@
 package com.example.coneeze.data
 
-import androidx.compose.ui.res.painterResource
 import com.example.coneeze.R
 
-val coffeeImages3 = listOf(
+val coffeImages4 = listOf(
     R.drawable.guatemala,
-    R.drawable.arabica,
-    R.drawable.ethi_co,
-    R.drawable.ethi_ari
+    R.drawable.arabica
 )
 
-
-val prices3 = listOf(
+val prices4 = listOf(
     "8,300원",
     "7,900원",
     "11,200원",
     "16,800원",
 )
 
-val names3 = listOf(
+val names4 = listOf(
     "과테말라 안티구아 SHB",
     "100 아라비카 블렌딩 롱로이",
     "에티오피아 코케허니 예가체프 G1 스페셜티",
@@ -26,7 +22,8 @@ val names3 = listOf(
 
     )
 
-val features3 = listOf(
+
+val features4 = listOf(
     "다크 초콜릿",
     "산미, 감귤",
     "고소, 견과류",
